e623b56383225d9eb137631efe8e213d2a5d5557
Karol Woźniak <VeXoN555@users.noreply.github.com>
2020-12-17 18:42:08 +0100

    Wpisanie kodu

--- plik.js
+++ plik.js
@@ -1,1 +1,50 @@
-document.write("Siema!!");
+let tab=[]
+for(let i=0;i<=5;i++)
+{
+    tab[i] = parseInt(prompt(`Podaj wiek ${i+1} osoby`));
+    if(tab[i]<=0)
+    {
+    do
+    {
+        tab[i] = parseInt(prompt(`Podaj poprawny wiek ${i+1} osoby`));
+    }while(tab[i]<=0)
+    }
+
+}
+
+let min=tab[0];
+let max=0;
+
+for(let i=0;i<=5;i++)
+{
+    if(tab[i]<min)
+    {
+        min=tab[i];
+    }
+    else if(tab[i]>max)
+    {
+        max=tab[i];
+    }
+}
+
+
+document.write(`<br>Najstarsza osoba ma: ${max} lat`);
+document.write(`<br>Najmłodsza osoba ma: ${min} lat`);
+
+for(let i=0;i<6-1;i++)
+{
+    for(let j=0;j<6-1-i;j++)
+    {
+        if(tab[j+1]<tab[j])
+        {
+            let x=tab[j+1];
+            tab[j+1]=tab[j];
+            tab[j]=x;
+        }
+    }
+}
+
+for(let i=0;i<6;i++)
+{
+    document.write(`${tab[i]} `)
+}
